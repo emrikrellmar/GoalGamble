87b04aa7768aadc6b26bff4f69ab25b56affab5f
emrikrellmar <emrik.rellmar@gmail.com>
2024-10-14 23:50:50 +0200

    Initial commit: Set up SvelteKit project

---
+++ www/public/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error Sökväg</title>
+    <style>
+        html{
+            font-size: 100%;
+        }
+
+        @media only screen and (max-width: 800px){
+            html{
+                font-size: 80%;
+            }
+        }
+
+        @media only screen and (max-width: 400px){
+            html{
+                font-size: 50%;
+            }
+        }
+
+        div{
+            position: relative;
+            text-align: center;
+        }
+
+        h1 {
+            width: fit-contet;
+            font-family: Roboto, sans-serif;
+            font-size: 10rem;
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        h1 span{
+            font-size: 6.5rem;
+        }
+        
+        h2{
+            font-family: Roboto, sans-serif;
+            font-size: 4rem;
+            display: inline-block;
+        }
+        
+    </style>
+</head>
+<body>
+    <div>
+        <h1>404 <span>Error</span></h1>
+        <h2>Felaktig sökväg</h2>
+    </div>
+</body>
+</html>
